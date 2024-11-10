52dbec259f2b4256f95266a99b369fb6f8e7ebf9
David Alejandro Llanes Bojorquez <david.llanes1019@gmail.com>
2024-11-10 16:36:23 -0700

    chore(client): agrega componentes y agrega configuraciones menores

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@routes': path.resolve(__dirname, './src/routes'),
     },
